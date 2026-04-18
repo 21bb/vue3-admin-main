7ba4e00a7587dd8dc201f5ae008786029dde2928
LiHua <2036881770@qq.com>
2026-04-18 09:57:30 +0800

    完成获取添加SKU数据展示的业务

--- src/views/product/spu/skuForm.vue
+++ src/views/product/spu/skuForm.vue
@@ -15,26 +15,50 @@
     <el-form-item label="SKU描述">
       <el-input placeholder="SKU描述" type="textarea"></el-input>
     </el-form-item>
-
     <!-- 平台属性 -->
     <el-form-item label="平台属性">
-      <el-form :inline="true">
-        <el-form-item v-for="(item,index)in attrArr" :key="item.id" :label="item.attrName">
-          <el-select style="width: 200px">
-            <el-option v-for="(attrValue,index)in item.attrValueList" :key="attrValue.id"></el-option>
+      <!-- 把 el-form 直接换成 div，去掉表单嵌套！！ -->
+      <div class="inline-flex flex-wrap gap-10">
+        <el-form-item v-for="(item) in attrArr" :key="item.id" :label="item.attrName">
+          <!-- 必须加 v-model -->
+          <el-select v-model="item.selectedValue" style="width: 200px" placeholder="请选择">
+
+            <!-- 内层循环不能用 index，改成 i -->
+            <el-option v-for="(attrValue, i) in item.attrValueList" :key="i" :label="attrValue.valueName"
+              :value="attrValue" />
+
           </el-select>
         </el-form-item>
-      </el-form>
-
+      </div>
     </el-form-item>
+    <!--销售属性-->
+    <el-form-item label="销售属性">
+      <div class="inline-flex flex-wrap gap-10">
+        <el-form-item v-for="(item) in saleArr" :key="item.id" :label="item.saleAttrName">
+          <!-- 必须加 v-model -->
+          <el-select style="width: 200px" placeholder="请选择">
 
+            <!-- 内层循环不能用 index，改成 i -->
+            <el-option v-for="(saleAttrValue, i) in item.spuSaleAttrValueList" :key="saleAttrValue.id"
+              :label="saleAttrValue.saleAttrValueName" />
+          </el-select>
+        </el-form-item>
+      </div>
+    </el-form-item>
     <!-- 图片列表 -->
     <el-form-item label="图片名称">
-      <el-table border>
+      <el-table border :data="imgArr">
         <el-table-column type="selection" width="80px" align="center"></el-table-column>
-        <el-table-column label="图片" prop="url"></el-table-column>
-        <el-table-column label="名称" prop="name"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="图片">
+          <template #="{ row, $index }">
+            <img :src="row.imgUrl" alt="" style="width:100px;height:100px;" </template>
+        </el-table-column>
+        <el-table-column label="名称" prop="imgName"></el-table-column>
+        <el-table-column label="操作">
+          <template #="{ row, $index }">
+            <el-button type="primary" size="small">设置默认</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-form-item>
 
@@ -51,7 +75,7 @@ import { ref, reactive } from 'vue'
 //引入请求api
 import { reqAttr } from '@/api/product/attr';
 import { reqSpuImageList, reqSpuHasSaleAttr } from '@/api/product/spu';
-
+let $emit = defineEmits(['changeScene']);
 //取消按钮的回调
 const cancel = () => {
   $emit('changeScene', { flag: 0, params: '' });
@@ -83,7 +107,7 @@ const initSkuData = async (c1Id: number | string, c2Id: number | string, spu: an
 defineExpose({
   initSkuData
 })
-let $emit = defineEmits(['changeScene']);
+
 </script>
 
 <style scoped></style>
